05fd3486faa5b3b7fc03443d8c8fc2efab34c966
Gaurab <gaurab.rpa@gmail.com>
2019-06-22 22:16:35 +0530

    New
    Changes

---
+++ FactorialRecursively.py
@@ -0,0 +1,14 @@
+def fact(number):
+    if number == 0:
+        return 1
+    else:
+        product = number * fact(number-1)
+        return product
+
+
+number2 = input("Enter number for factorial: ")
+
+if number2.isnumeric():
+    print(fact(int(number2)))
+else:
+    print("Enter a valid number")
